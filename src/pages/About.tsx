bac19ccb36bf0e128cf4402d75530cfb2a4ccf0f
vwc inc <vwccompanies23@email.com>
2026-05-12 13:03:38 -0400

    Fix X icon case

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ import { useLanguage } from '../LanguageContext';
 import youtubeIcon from '../assets/icons/youtube.png';
 import facebookIcon from '../assets/icons/facebook.png';
 import instagramIcon from '../assets/icons/instagram.png';
-import xIcon from '../assets/icons/x.png';
+import xIcon from '../assets/icons/X.png';
 import logo from '../assets/kasuku-logo.png';
 
 export default function About() {
